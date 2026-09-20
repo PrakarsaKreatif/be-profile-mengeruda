98666d3dde672304d976e0f2eb98f35b5ea15cc7
Thobiq <malyasyithobiq@gmail.com>
2026-09-20 21:16:55 +0700

    fixing

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,5 +5,4 @@ use App\Providers\AppServiceProvider;
 return [
     App\Providers\AppServiceProvider::class,
     Illuminate\View\ViewServiceProvider::class,
-    Dedoc\Scramble\ScrambleServiceProvider::class,
 ];
