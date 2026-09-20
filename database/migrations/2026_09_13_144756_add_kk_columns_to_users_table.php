86db2269b5e121e96a3fb0fd48734dce34ee7539
Thobiq <malyasyithobiq@gmail.com>
2026-09-20 19:55:06 +0700

    fixing urutan migrasi database

--- database/migrations/2026_09_13_144756_add_kk_columns_to_users_table.php
+++ database/migrations/2026_09_13_144756_add_kk_columns_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('is_approved')->default(false)->after('phone');
+            $table->boolean('is_approved')->default(false)->after('email');
             $table->string('kk_path')->nullable();
             $table->boolean('is_kk_approved')->default(false)->after('kk_path');
         });
