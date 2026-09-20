51cf7bce8b393fd684fbf43f345cb8eb513b6699
Thobiq <malyasyithobiq@gmail.com>
2026-09-20 20:46:41 +0700

    trying to fixing error cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,10 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
-            fn (Request $request) => $request->is('api/*'),
+            fn (Request $request) => true,
         );
-    })->create();
+    })
+    ->withProviders([
+        \Illuminate\View\ViewServiceProvider::class,
+    ])
+    ->create();
